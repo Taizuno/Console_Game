6eeb0cea043942010550239a29ac3bebdf71037f
Braeden McNealy <lightning9501@gmail.com>
2022-06-16 18:37:50 -0400

    Armor_Repo

--- src/Game.Repository/Repositories/Armor_Repo.cs
+++ src/Game.Repository/Repositories/Armor_Repo.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Game.Repository.Repositories
-{
+//namespace Game.Repository.Repositories
+
     public class Armor_Repo
     {
-        
+        private readonly List<Armor> _armorRepo = new List<Armor>();
+        private int _count;
+
+        public bool addWeaponToDB(Armor tool)
+        {
+            if(tool != null)
+            {
+            _count++;
+            tool.ID = _count;
+            _armorRepo.Add(tool);
+            return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public List<Armor> ViewAllArmor()
+        {
+            return _armorRepo;
+        }
+        public Armor ViewArmorByID(int id)
+        {
+            foreach(Armor a in _armorRepo)
+            {
+                if(a.ID == id)
+                {
+                    return a;
+                }
+            }
+            return null;
+        }   
     }
-}
