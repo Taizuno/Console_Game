85275d8c8083eddc58977ed2265e0a6664184f14
Braeden McNealy <lightning9501@gmail.com>
2022-06-16 18:27:20 -0400

    Weapon_Repo

--- src/Game.Repository/Repositories/Weapon_Repo.cs
+++ src/Game.Repository/Repositories/Weapon_Repo.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Game.Repository.Repositories
-{
+//namespace Game.Repository.Repositories
+
     public class Weapon_Repo
     {
-        
+        private readonly List<Weapon> _weaponRepo = new List<Weapon>();
+        private int _count;
+
+        public bool addWeaponToDB(Weapon tool)
+        {
+            if(tool != null)
+            {
+            _count++;
+            tool.ID = _count;
+            _weaponRepo.Add(tool);
+            return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public List<Weapon> ViewAllWeapons()
+        {
+            return _weaponRepo;
+        }
+        public Weapon ViewWeaponByID(int id)
+        {
+            foreach(Weapon w in _weaponRepo)
+            {
+                if(w.ID == id)
+                {
+                    return w;
+                }
+            }
+            return null;
+        }
     }
-}
